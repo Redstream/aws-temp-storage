fd36850c788f7dc22968b7f6fcb3277c365bd525
Gustav Rödström <gustav.rodstrom@gmail.com>
2018-08-17 12:21:29 -0400

    stuff

--- lambda-backend/app.js
+++ lambda-backend/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
-app.get('/mirror', (req, res) => res.send(req.toSource()));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => res.send("Hello World!"));
+app.all('/mirror', (req, res) => res.send({query: req.query, headers: req.headers, body: req.body}));
 
 module.exports = app;
